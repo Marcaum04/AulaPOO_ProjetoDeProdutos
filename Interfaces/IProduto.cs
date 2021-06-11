7749c1322da51aa5b0214f87479ea3ed5272e789
Rafolios <rapaeltavares1988@gmail.com>
2021-06-11 18:03:31 -0300

    Produto/Marca prontos

--- Interfaces/IProduto.cs
+++ Interfaces/IProduto.cs
@@ -1,7 +1,13 @@
+using Projetos_Produtos.Classes;
+
 namespace Projetos_Produtos.Interfaces
 {
     public interface IProduto
     {
-         
+         string Cadastrar(Produto produto);
+
+         string Deletar(Produto produto);
+
+         void Listar();
     }
 }
